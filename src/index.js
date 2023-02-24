4c05fe51910e4ae9a9bb22bbd3be68b97b192f2b
dimsniko <dimsniko@gmail.com>
2023-02-24 17:33:54 +0400

    ready

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@
 
 module.exports = function towelSort (matrix) {
   return [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0) {
+      XPathResult.push(...matrix[i])
+    } else {
+      XPathResult.push(...matrix[i].reverse())
+    }
+  }
+  return result
 }
